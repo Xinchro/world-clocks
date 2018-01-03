e4b489b07caed8c57fd430cd30b87f8fd8bff2f4
Xinchro <TheXrocks@gmail.com>
2018-01-03 16:04:59 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let app = new Vue({
+  el: '#vue-app',
+  data: {
+    message: 'Hello Vue.js!'
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
+})
+
+// launch app
+app.start()
